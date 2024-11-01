3bf4d28d25989514f3c2e7364089c7e8f60fe341
Doniyor Eshmamatov <edoniyorbekdev@gmail.com>
2024-11-01 15:59:20 +0500

    eqweeeq

--- repository/repository.js
+++ repository/repository.js
@@ -1,12 +1,27 @@
 import axios from "axios";
+import https from "https";
 
-export const baseDomain = 'https://admin.soffhub.uz/api/v1/';
+export const baseDomain = "https://admin.soffhub.uz/api/v1/";
 // export const baseDomain = 'http://192.168.1.15:8000/api/v1/';
 
+const cert = process.env.NEXT_PUBLIC_CLIENT_CERT.replace(/\\n/g, "\n");
+const key = process.env.NEXT_PUBLIC_CLIENT_KEY.replace(/\\n/g, "\n");
+const ca = process.env.NEXT_PUBLIC_ROOTCA_PEM.replace(/\\n/g, "\n");
+
+export const httpsAgent = new https.Agent({
+  cert,
+  key,
+  ca,
+});
 
 const clinet = axios.create({
   baseURL: baseDomain,
-  headers: "Header",
+});
+
+clinet.interceptors.request.use((config) => {
+  config.httpsAgent = httpsAgent;
+
+  return config;
 });
 
 export default clinet;
